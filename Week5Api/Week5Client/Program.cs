e1f2b346abd03395743c53a3b01566075495076a
riyabbr5-coder <riyabbr5@gmail.com>
2026-02-14 09:16:56 -0500

    Updated hello endpoint for CI test

---
+++ Week5Api/Week5Client/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http;
+
+var client = new HttpClient();
+
+string url = "https://lab-week5api-rb-1.onrender.com/api/Hello";
+
+Console.WriteLine("Calling API...");
+string response = await client.GetStringAsync(url);
+
+Console.WriteLine("Response:");
+Console.WriteLine(response);
