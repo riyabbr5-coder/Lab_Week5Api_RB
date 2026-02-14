32d3a1913a17deccc3110056419edcd26f79fdef
riyabbr5-coder <riyabbr5@gmail.com>
2026-02-14 16:33:01 -0500

    updated program.cs

--- Week5Api/Program.cs
+++ Week5Api/Program.cs
@@ -1,20 +1,4 @@
-
-using System.Net.Http;
-
-var client = new HttpClient();
-
-// Use your LIVE Render API URL
-string url = "https://lab-week5api-rb-1.onrender.com/api/Hello";
-
-Console.WriteLine("Calling API...");
-string response = await client.GetStringAsync(url);
-
-Console.WriteLine("Response:");
-Console.WriteLine(response);
-
-
-
-/*using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 namespace Week5Api;
@@ -54,6 +38,6 @@ public class Program
         app.Run();
     }
 }
-*/
 
 
+    
